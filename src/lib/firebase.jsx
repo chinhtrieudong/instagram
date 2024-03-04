60b281827fc082a6fd07cfd331fac04fb69244bf
chinhtrieudong <86337310+chinhtrieudong@users.noreply.github.com>
2024-03-04 21:23:05 +0700

    Update firebase.jsx

--- src/lib/firebase.jsx
+++ src/lib/firebase.jsx
@@ -5,12 +5,12 @@ import 'firebase/compat/auth';
 // import { seedDatabase } from '~/seed';
 
 const config = {
-    apiKey: 'AIzaSyBNClFRFHz76JzaHckEg6aI6N9RhYjg8r0',
-    authDomain: 'instagram-web-f9dff.firebaseapp.com',
-    projectId: 'instagram-web-f9dff',
-    storageBucket: 'instagram-web-f9dff.appspot.com',
-    messagingSenderId: '821064868643',
-    appId: '1:821064868643:web:78bb218f18c9e15a316296',
+    apiKey,
+    authDomain,
+    projectId,
+    storageBucket,
+    messagingSenderId,
+    appId,
 };
 
 const firebaseApp = firebase.initializeApp(config);

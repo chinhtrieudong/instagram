de1b7c6e03be5d5a396bedef9558a1aae698583b
chinhtrieudong <86337310+chinhtrieudong@users.noreply.github.com>
2024-03-05 08:41:01 +0700

    Update login.jsx

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -37,7 +37,7 @@ const Login = () => {
         <div className="container flex mx-auto max-w-screen-md items-center h-screen ">
             <div className="flex w-3/5">
                 <img
-                    src="/images/iphone-with-profile.jpg"
+                    src="images/iphone-with-profile.JPG"
                     alt="iphone with instagram"
                 />
             </div>
